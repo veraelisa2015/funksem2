9c763284d0a83c4da17e3e85e6cac5fb0be2615b
veraelisa2015 <veralein2014@gmx.de>
2015-05-19 14:22:49 +0200

    Update eintrag.java
    hab mal die @authors eingefügt

--- funkTest/eintrag.java
+++ funkTest/eintrag.java
@@ -1,5 +1,16 @@
 package funkTest;
-
+/**
+ * Class for all entries. 
+ * This class has no general functions.
+ * 
+ * @author Vera Gögelein 9267625
+ * @author Simon Bayer 5601000
+ * @author Robert Leipelt 9469264
+ * @author Patrice Bender 1176484
+ * 
+ * @version 1.0
+ *
+ */
 public class eintrag {
 
 	private String notiz;
